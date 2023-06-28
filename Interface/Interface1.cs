639548c66bcf9ac0f2aeb76ccc5a30df07a2a9a9
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-28 12:33:31 +0530

    Add project files.

---
+++ Interface/Interface1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceClass
+{
+    public interface IPrint
+    {
+        string PrintD();
+    }
+
+    abstract public class Shape
+    {
+        public abstract void CalculateArea();
+    }
+
+    public class Circle : Shape, IPrint
+    {
+        private int r;
+        private double area;
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+        public override void CalculateArea()
+        {
+            area = 3.14 * r * r;
+        }
+        public string PrintD()
+        {
+            return $"Area Of Circle = {area}";
+        }
+
+        public override string ToString()
+        {
+            return $"Area Of Circle = {area}";
+        }
+    }
+
+    public class Rectangle : Shape, IPrint
+    {
+        private int l, b;
+        private double area;
+
+        public Rectangle(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public override void CalculateArea()
+        {
+            area = l * b;
+        }
+
+        public string PrintD()
+        {
+            return $"Area of Reactangle = {area}";
+        }
+
+        public override string ToString()
+        {
+            return $"Area of Reactangle = {area}";
+        }
+    }
+}
+
---
+++ Interface/Program.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(10);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+            Rectangle rect = new Rectangle(5, 6);
+            rect.CalculateArea();
+            Console.WriteLine(rect);
+        }
+    }
+}
